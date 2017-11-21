4d950c9690848c278231c7942c2f82fca7b3d385
yaroslavdus <yaroslavdus@gmail.com>
2017-11-20 21:05:54 -0800

    modified test

--- src/test/java/VerifyTitle.java
+++ src/test/java/VerifyTitle.java
@@ -2,17 +2,27 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class VerifyTitle {
 	
 	static WebDriver driver;
 	
+	@BeforeMethod
+	public void setup() {
+		
+		String browser = "Chrome";
+		
+		if(browser.equalsIgnoreCase("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
+			driver = new ChromeDriver();
+		}
+	}
+	
 	@Test
 	public void test_01() {
 		
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
-		driver = new ChromeDriver();
 		driver.get("http://learn-automation.com");
 		Assert.assertTrue(driver.getTitle().contains("Selenium"));
 		
